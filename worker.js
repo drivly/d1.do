b4f96a33b69fbe1389798e40578572516b9e6137
Nathan Clevenger <nateclev@gmail.com>
2022-08-02 06:59:54 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,1 +1,8 @@
-
+export default {
+ fetch: async (req, env) => {
+   const { pathname, searchParams } = new URL(req.url)
+   const [ table ] = pathname.split('/')
+   const data = await db.prepare('SELECT * FROM ?1').bind(table).all()
+   return new Response(JSON.stringify({data}), null, 2, { headers: { 'content-type': 'application/json' }})
+ }
+}
