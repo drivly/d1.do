a50fa93d24e4db303ec7eded1c03ee1b5edba17d
Nathan Clevenger <nateclev@gmail.com>
2022-08-02 07:07:02 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,8 +1,13 @@
+const json = data => new Response(JSON.stringify({data}), null, 2, { headers: { 'content-type': 'application/json' }})
+
 export default {
  fetch: async (req, env) => {
    const { pathname, searchParams } = new URL(req.url)
    const [ table ] = pathname.split('/')
-   const data = await db.prepare('SELECT * FROM ?1').bind(table).all()
-   return new Response(JSON.stringify({data}), null, 2, { headers: { 'content-type': 'application/json' }})
+//    if (!table) {
+//     `SELECT name FROM sqlite_schema WHERE type='table' ORDER BY name` 
+//    }
+   const data = await env.DB.prepare('SELECT * FROM ?1').bind(table).all()
+   return json(data)
  }
 }
